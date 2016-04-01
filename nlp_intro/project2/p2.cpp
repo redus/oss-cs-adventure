09e05cace82f32abe83c5b0590fcb3d5f090a201
Jin Yang <redus891@gmail.com>
2016-04-02 00:50:33 +0900

    json parsing bit

--- nlp_intro/project2/p2.cpp
+++ nlp_intro/project2/p2.cpp
@@ -1,10 +1,36 @@
-#include <json/json.h>
+#include "json/json.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <unordered_map>
+
+
+
+// EFF: replace rare(< 5 occurrences)
+void replace_rare(){
+	// construct json from raw json data
+	Json::Value root;
+	std::ifstream json, original;
+	std::ofstream copy;
+	json.open("parse_train.json");
+	original.open("parse_train.dat")
+
+	Json::CharReaderBuilder rbuilder;
+	rbuilder["collectComments"] = false;
+	rbuilder["strictRoot"] = false;
+	std::string errs;
+	Json::parseFromStream(rbuilder, fin, &root, &errs);
+	
+	fin.close();
+	// count all the word ocurrences into hash (key: word, val: count)
+	// filter the hash for less than 5 occurrences
+	// if there's a match in parse_train.dat
+
+
+	
+}
 
 int main(){
-	json::Value root;
-	std::cout << "input tree sample\n";
-	std::cin >> root;
-	std::cout << root << std::endl;
-
+	replace_rare();
 	return 0;
 }
